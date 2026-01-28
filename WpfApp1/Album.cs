10e0be2df847cebb883b0520d25acf5bd6a1689d
Luc Deldem <S00295653@atu.ie>
2026-01-28 09:48:23 +0000

    Add project files.

---
+++ WpfApp1/Album.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Album
+    {
+
+        public string Name { get; set; }
+        public int YearOfRelease { get; set; }
+        public int Sales { get; set; }
+
+        public Album(string name, int yearOfRelease, int sales)
+        {
+            Name = name;
+            YearOfRelease = yearOfRelease;
+            Sales = sales;
+        }
+
+        public Album()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"{Name} - released for { DateTime.Now.Year - YearOfRelease} years. Sales of  {Sales}");
+        }
+    }
+}
---
+++ WpfApp1/Band.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public abstract class Band : IComparable
+    {
+        public string BandName { get; set; }
+        public int YearFormed { get; set; }
+        public string Members { get; set; }
+        public List<Album> AlbumList { get; set; }
+
+
+        public Band(string bandName, int yearFormed, string members)
+        {
+            BandName = bandName;
+            YearFormed = yearFormed;
+            Members = members;
+
+            AlbumList = new List<Album>();
+        }
+
+        public Band() : this("Unknown", 1960, "Unknown") { }
+
+        public override string ToString()
+        {
+            return BandName;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Band otherband = obj as Band;
+            return this.BandName.CompareTo(otherband.BandName);
+        }
+    }
+
+    public class RockBand : Band
+    {
+        public override string ToString()
+        {
+            return base.ToString() + " - Rock Band";
+        }
+    }
+
+    public class PopBand : Band
+    {
+        public override string ToString()
+        {
+            return base.ToString() + " - Pop Band";
+        }
+    }
+
+    public class IndieBand : Band
+    {
+        public override string ToString()
+        {
+            return base.ToString() + " - Indie Band";
+        }
+    }
+}
